1d1f611093c2a90cab2b4b372f40ec159a898911
Snitrxm <andre.oliveira.roch@hotmail.com>
2026-01-27 22:17:41 +0000

    a

--- app/[locale]/imoveis/page.tsx
+++ app/[locale]/imoveis/page.tsx
@@ -108,86 +108,86 @@ function ImoveisContent() {
     };
 
     return (
-            <section className="h-[calc(100vh-64px)] xl:h-[calc(100vh-72px)] overflow-hidden">
-                <div className="container flex flex-col lg:flex-row lg:divide-x divide-[#EAE6DF] h-full overflow-hidden">
-                    <Sidebar isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)} />
-                    <div className="border-r border-[#EAE6DF] bg-deaf w-full flex flex-col overflow-hidden min-h-0">
-                        <div className="px-4 md:px-6 py-4 flex flex-col sm:flex-row gap-4 sm:justify-between border-b border-[#EAE6DF] shrink-0">
-                            <div className="flex items-center gap-4">
+        <section className="h-[calc(100vh-64px)] xl:h-[calc(100vh-72px)] overflow-hidden">
+            <div className="container flex flex-col lg:flex-row lg:divide-x divide-[#EAE6DF] h-full overflow-hidden">
+                <Sidebar isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)} />
+                <div className="border-r border-[#EAE6DF] bg-deaf w-full flex flex-col overflow-hidden min-h-0">
+                    <div className="px-4 md:px-6 py-4 flex flex-col sm:flex-row gap-4 sm:justify-between border-b border-[#EAE6DF] shrink-0">
+                        <div className="flex items-center gap-4">
+                            <button
+                                onClick={() => setSidebarOpen(true)}
+                                className="xl:hidden bg-white shadow-pretty p-1.5 cursor-pointer hover:bg-deaf text-black-muted body-14-medium flex items-center gap-1">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none" className="text-grey">
+                                    <path d="M8 4V16M16.4 4H3.6C2.71634 4 2 4.59695 2 5.33333V14.6667C2 15.4031 2.71634 16 3.6 16H16.4C17.2837 16 18 15.4031 18 14.6667V5.33333C18 4.59695 17.2837 4 16.4 4Z" stroke="currentColor" strokeWidth="1.25" strokeLinejoin="round" />
+                                </svg>
+                                {t("filter")}
+                            </button>
+                            <div className="bg-white shadow-pretty divide-x divide-muted">
                                 <button
-                                    onClick={() => setSidebarOpen(true)}
-                                    className="xl:hidden bg-white shadow-pretty p-1.5 cursor-pointer hover:bg-deaf text-black-muted body-14-medium flex items-center gap-1">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none" className="text-grey">
-                                        <path d="M8 4V16M16.4 4H3.6C2.71634 4 2 4.59695 2 5.33333V14.6667C2 15.4031 2.71634 16 3.6 16H16.4C17.2837 16 18 15.4031 18 14.6667V5.33333C18 4.59695 17.2837 4 16.4 4Z" stroke="currentColor" strokeWidth="1.25" strokeLinejoin="round" />
+                                    onClick={() => handlePageChange(currentPage - 1)}
+                                    disabled={currentPage === 1}
+                                    className="cursor-pointer p-1.5 hover:bg-deaf disabled:opacity-40 disabled:cursor-not-allowed"
+                                >
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none" className="text-black-grey">
+                                        <path d="M8.33333 4.79163L3.125 9.99996L8.33333 15.2083M3.75 9.99996H16.875" stroke="currentColor" strokeWidth="1.66667" strokeLinecap="round" strokeLinejoin="round" />
                                     </svg>
-                                    {t("filter")}
                                 </button>
-                                <div className="bg-white shadow-pretty divide-x divide-muted">
-                                    <button
-                                        onClick={() => handlePageChange(currentPage - 1)}
-                                        disabled={currentPage === 1}
-                                        className="cursor-pointer p-1.5 hover:bg-deaf disabled:opacity-40 disabled:cursor-not-allowed"
-                                    >
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none" className="text-black-grey">
-                                            <path d="M8.33333 4.79163L3.125 9.99996L8.33333 15.2083M3.75 9.99996H16.875" stroke="currentColor" strokeWidth="1.66667" strokeLinecap="round" strokeLinejoin="round" />
-                                        </svg>
-                                    </button>
-                                    <button
-                                        onClick={() => handlePageChange(currentPage + 1)}
-                                        disabled={currentPage === totalPages}
-                                        className="cursor-pointer p-1.5 hover:bg-deaf disabled:opacity-40 disabled:cursor-not-allowed"
-                                    >
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none" className="text-black-muted">
-                                            <path d="M11.6667 4.79163L16.875 9.99996L11.6667 15.2083M16.25 9.99996H3.125" stroke="currentColor" strokeWidth="1.66667" strokeLinecap="round" strokeLinejoin="round" />
-                                        </svg>
-                                    </button>
-                                </div>
-                                <p className="text-[15px] text-black-muted"><span>{startItem} - {endItem}</span> {t("of")} <span>{totalItems}</span> {t("properties")}</p>
+                                <button
+                                    onClick={() => handlePageChange(currentPage + 1)}
+                                    disabled={currentPage === totalPages}
+                                    className="cursor-pointer p-1.5 hover:bg-deaf disabled:opacity-40 disabled:cursor-not-allowed"
+                                >
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none" className="text-black-muted">
+                                        <path d="M11.6667 4.79163L16.875 9.99996L11.6667 15.2083M16.25 9.99996H3.125" stroke="currentColor" strokeWidth="1.66667" strokeLinecap="round" strokeLinejoin="round" />
+                                    </svg>
+                                </button>
                             </div>
-                            <div className="flex items-center gap-2">
-                                <Label htmlFor="tipo" className="body-14-medium text-grey whitespace-nowrap">{t("sortBy")}</Label>
-                                <Select value={getSortLabel(filters.sortBy || "-createdAt")} onValueChange={handleSortChange}>
-                                    <SelectTrigger id="tipo" name="tipo">
-                                        <SelectValue placeholder={t("sortOptions.mostRecent")} />
-                                    </SelectTrigger>
-                                    <SelectContent>
-                                        <SelectItem value="mais-recentes">{t("sortOptions.mostRecent")}</SelectItem>
-                                        <SelectItem value="mais-antigos">{t("sortOptions.oldest")}</SelectItem>
-                                        <SelectItem value="menor-preco">{t("sortOptions.lowestPrice")}</SelectItem>
-                                        <SelectItem value="maior-preco">{t("sortOptions.highestPrice")}</SelectItem>
-                                    </SelectContent>
-                                </Select>
-                            </div>
+                            <p className="text-[15px] text-black-muted"><span>{startItem} - {endItem}</span> {t("of")} <span>{totalItems}</span> {t("properties")}</p>
                         </div>
-                        <div className="flex-1 overflow-y-auto min-h-0">
-                            {isLoading ?
+                        <div className="flex items-center gap-2">
+                            <Label htmlFor="tipo" className="body-14-medium text-grey whitespace-nowrap">{t("sortBy")}</Label>
+                            <Select value={getSortLabel(filters.sortBy || "-createdAt")} onValueChange={handleSortChange}>
+                                <SelectTrigger id="tipo" name="tipo">
+                                    <SelectValue placeholder={t("sortOptions.mostRecent")} />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectItem value="mais-recentes">{t("sortOptions.mostRecent")}</SelectItem>
+                                    <SelectItem value="mais-antigos">{t("sortOptions.oldest")}</SelectItem>
+                                    <SelectItem value="menor-preco">{t("sortOptions.lowestPrice")}</SelectItem>
+                                    <SelectItem value="maior-preco">{t("sortOptions.highestPrice")}</SelectItem>
+                                </SelectContent>
+                            </Select>
+                        </div>
+                    </div>
+                    <div className="flex-1 overflow-y-auto min-h-0">
+                        {isLoading ?
                             (<div className="grid place-content-center h-full min-h-[400px]">
                                 <p>{t("loading")}</p>
                             </div>) : error ?
-                            (<div className="grid place-content-center h-full min-h-[400px]">
-                                <p>{t("error")}</p>
-                            </div>) : !filteredData || filteredData.data.length === 0 ?
-                            (<div className="grid place-content-center h-full min-h-[400px] text-center p-4">
-                                <p className="body-16-medium text-brown">{t("noResults")}</p>
-                                <p className="body-14-regular mt-1 w-80 text-grey">{t("noResultsDescription")}</p>
-                            </div>) : (
-                            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
-                                {filteredData.data.map((property) => (
-                                    <Card
-                                        key={property.id}
-                                        image={property.image}
-                                        href={`/${locale}/imoveis/${property.id}`}
-                                        titulo={property.title}
-                                        localizacao={`${property.concelho}, ${property.distrito}`}
-                                        preco={property.price}
-                                        status={property.status}
-                                    />))}
-                            </div>
-                            )}
-                        </div>
+                                (<div className="grid place-content-center h-full min-h-[400px]">
+                                    <p>{t("error")}</p>
+                                </div>) : !filteredData || filteredData.data.length === 0 ?
+                                    (<div className="grid place-content-center h-full min-h-[400px] text-center p-4">
+                                        <p className="body-16-medium text-brown">{t("noResults")}</p>
+                                        <p className="body-14-regular mt-1 w-80 text-grey">{t("noResultsDescription")}</p>
+                                    </div>) : (
+                                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+                                            {filteredData.data.map((property) => (
+                                                <Card
+                                                    key={property.id}
+                                                    image={property.image}
+                                                    href={`/${locale}/imoveis/${property.id}`}
+                                                    titulo={property.title}
+                                                    localizacao={`${property.concelho}, ${property.distrito}`}
+                                                    preco={property.price}
+                                                    status={property.status}
+                                                />))}
+                                        </div>
+                                    )}
                     </div>
                 </div>
-            </section>
+            </div>
+        </section>
     );
 }
 
